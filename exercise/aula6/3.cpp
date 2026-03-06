eacf901e0f6c090ab2186bd22c695ef6b6258ddf
GussApenas <gussapenasofc@gmail.com>
2026-03-05 21:23:39 -0300

    add exercicio 3

---
+++ exercise/aula6/3.cpp
@@ -0,0 +1,19 @@
+/*
+proposta gerada pelo GEMINI.
+3. Média Ponderada "na mão"
+  - Global: int p1 = 10, p2 = 2; (pesos das notas)
+  - Local: int n1 = 8, n2 = 5, media;
+  - Tarefa: Calcule a média usando a fórmula: media=((numero1 * prova1) + (numero2 * prova2)) / (prova1 + prova2) Exiba apenas o valor inteiro da média.
+*/
+
+int p1 = 10, p2 = 2;
+
+int main() {
+  int n1 = 8, n2 = 5, media;
+
+  media = ((n1 * p1) + (n2 * p2)) / (p1 + p2)
+
+  cout << media;
+  
+  return 0;
+}
